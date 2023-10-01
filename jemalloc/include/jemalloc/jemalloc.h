dbefd8492470fe6accdf9d823e4be4c3e9c04b06
Akilan <me@akilan.io>
2023-10-01 15:55:24 +0100

    changes

--- jemalloc/include/jemalloc/jemalloc.h
+++ jemalloc/include/jemalloc/jemalloc.h
@@ -224,6 +224,8 @@ extern JEMALLOC_EXPORT const char	*je_malloc_conf;
 extern JEMALLOC_EXPORT void		(*je_malloc_message)(void *cbopaque,
     const char *s);
 
+#define jemalloc(size)
+
 JEMALLOC_EXPORT JEMALLOC_ALLOCATOR JEMALLOC_RESTRICT_RETURN
     void JEMALLOC_NOTHROW	*je_malloc(size_t size)
     JEMALLOC_CXX_THROW JEMALLOC_ATTR(malloc) JEMALLOC_ALLOC_SIZE(1);
