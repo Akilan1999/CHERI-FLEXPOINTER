a1dcae8e75f12a297371fc4475e4d30ee981b0dd
Akilan <me@akilan.io>
2024-01-02 01:02:41 +0000

    test hugeTLB

--- example/test-contig-mmap.c
+++ example/test-contig-mmap.c
@@ -16,7 +16,7 @@ int main(void) {
         void *mem_ro, *mem_rw;
 
         // Create a memfd
-        memfd = memfd_create("something", MFD_HUGETLB);
+        memfd = memfd_create("something", MFD_HUGE_1MB);
         if (memfd == -1) {
                 perror("memfd_create failed");
                 return 1;
