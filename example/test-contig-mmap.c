532ef23e3ba71d7c07c8c2b7e4130f65eb3be7de
Akilan <me@akilan.io>
2024-01-03 00:03:36 +0000

    contigous testing

--- example/test-contig-mmap.c
+++ example/test-contig-mmap.c
@@ -46,7 +46,7 @@ int main(void) {
         void *mem_ro, *mem_rw; 
 
         // Create a memfd
-        memfd = memfd_create_test("something", 0);
+        memfd = memfd_create_test("something", MFD_HUGETLB);
         if (memfd == -1) {
                 perror("memfd_create failed");
                 return 1;
