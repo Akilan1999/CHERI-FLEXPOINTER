5b824b3336977321fea0ce90dbfb482cdfe324c6
Akilan <me@akilan.io>
2024-01-03 01:11:02 +0000

    contigous testing

--- example/test-contig-mmap.c
+++ example/test-contig-mmap.c
@@ -61,7 +61,7 @@ int main(void) {
         // Give the file a size, otherwise reading/writing will fail
         if (ftruncate(memfd, 0x1000) == -1) {
                 perror("ftruncate failed");
-                return 1;
+                return 1;  
         }
 
         // Map the fd as read only and private
