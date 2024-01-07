584eafc6b47cea3c8a17afbccb20c39e4d8f700f
Akilan <me@akilan.io>
2024-01-07 23:46:27 +0000

    test contig malloc call

--- example/contigmalloc.c
+++ example/contigmalloc.c
@@ -1,8 +1,9 @@
 #include	<sys/types.h>
 #include	<sys/malloc.h>
 
-
+MALLOC_DEFINE(M_CONTIGMEM, "contigmem", "contigmem(4) allocations");
 static int64_t     contigmem_buffer_size = RTE_CONTIGMEM_DEFAULT_BUF_SIZE;
+#define	BUS_SPACE_MAXADDR	0xFFFFFFFF
 
 // Writing a sample contig malloc 
 // function to see if it can 
