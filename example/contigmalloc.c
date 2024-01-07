38383076b3d1dad97b4dedf827d98a651a4e9578
Akilan <me@akilan.io>
2024-01-07 23:48:55 +0000

    test contig malloc call

--- example/contigmalloc.c
+++ example/contigmalloc.c
@@ -1,5 +1,31 @@
-#include	<sys/types.h>
-#include	<sys/malloc.h>
+#include <sys/param.h>
+#include <sys/bio.h>
+#include <sys/bus.h>
+#include <sys/conf.h>
+#include <sys/kernel.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/proc.h>
+#include <sys/lock.h>
+#include <sys/rwlock.h>
+#include <sys/mutex.h>
+#include <sys/systm.h>
+#include <sys/sysctl.h>
+#include <sys/vmmeter.h>
+#include <sys/eventhandler.h>
+
+#include <machine/bus.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+#include <vm/vm_param.h>
+#include <vm/vm_object.h>
+#include <vm/vm_page.h>
+#include <vm/vm_pager.h>
+#include <vm/vm_phys.h>
+
+
+
 
 #define RTE_CONTIGMEM_DEFAULT_BUF_SIZE (512*1024*1024)
 static int64_t     contigmem_buffer_size = RTE_CONTIGMEM_DEFAULT_BUF_SIZE;
