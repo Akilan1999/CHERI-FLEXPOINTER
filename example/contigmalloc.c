13fae5866cdf6cc196ee8e64312c07bba3254d61
Akilan <me@akilan.io>
2024-01-08 12:57:22 +0000

    supress warning flag by passoing function shape

--- example/contigmalloc.c
+++ example/contigmalloc.c
@@ -36,7 +36,8 @@ __FBSDID("$FreeBSD$");
 #define RTE_CONTIGMEM_DEFAULT_BUF_SIZE (512*1024*1024)
 static int64_t     contigmem_buffer_size = RTE_CONTIGMEM_DEFAULT_BUF_SIZE;
 #define	BUS_SPACE_MAXADDR	0xFFFFFFFF
-MALLOC_DEFINE(M_DEVBUF, "devbuf", "device driver memory");
+// MALLOC_DEFINE(M_DEVBUF, "devbuf", "device driver memory");
+MALLOC_DEFINE(M_SMMU, "SMMU", SMMU_DEVSTR);
 // MALLOC_DEFINE(M_CONTIGMEM, "contigmem", "contigmem(4) allocations");
 
 // Writing a sample contig malloc 
