2e0a0d431e76e93590a503ac571b033fc743a842
Akilan <me@akilan.io>
2024-01-08 19:18:34 +0000

    supress warning flag by passoing function shape

--- example/contigmalloc.c
+++ example/contigmalloc.c
@@ -47,13 +47,25 @@
 	    vm_paddr_t boundary) __malloc_like __result_use_check
 	    __alloc_size(1) __alloc_align(6);
 
-char M_MYDRIVER;
-MALLOC_DEFINE(M_MYDRIVER, "devbuf", "buffers used by my driver");
+#define	MALLOC_DEFINE(type, shortdesc, longdesc)			\
+	struct malloc_type type[1] = {					\
+		{							\
+			.ks_next = NULL,				\
+			.ks_version = M_VERSION,			\
+			.ks_shortdesc = shortdesc,			\
+		}							\
+	};								\
+	SYSINIT(type##_init, SI_SUB_KMEM, SI_ORDER_THIRD, malloc_init,	\
+	    type);							\
+	SYSUNINIT(type##_uninit, SI_SUB_KMEM, SI_ORDER_ANY,		\
+	    malloc_uninit, type)
+
+// MALLOC_DEFINE(M_MYDRIVER, "devbuf", "buffers used by my driver");
 
 // #define RTE_CONTIGMEM_DEFAULT_BUF_SIZE (512*1024*1024)
 // static int64_t     contigmem_buffer_size = RTE_CONTIGMEM_DEFAULT_BUF_SIZE;
 // #define	BUS_SPACE_MAXADDR	0xFFFFFFFF
-// MALLOC_DEFINE(M_DEVBUF, "devbuf", "device driver memory");
+MALLOC_DEFINE(M_DEVBUF, "devbuf", "device driver memory");
 // MALLOC_DEFINE(M_SMMU, "SMMU", SMMU_DEVSTR);
 // MALLOC_DEFINE(M_CONTIGMEM, "contigmem", "contigmem(4) allocations");
 
@@ -62,7 +74,7 @@ MALLOC_DEFINE(M_MYDRIVER, "devbuf", "buffers used by my driver");
 // allocate contigous memory
 int main(void) {
     void *addr;
-    addr = contigmalloc(8192, M_MYDRIVER,	M_ZERO,	0, (1L << 22),
+    addr = contigmalloc(8192, M_DEVBUF,	M_ZERO,	0, (1L << 22),
 	   32 *	1024, 1024 * 1024);
 }
 
