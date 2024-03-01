c3f81c9cf01f2d0de42f5cd136e66545e9528c2f
Akilan <me@akilan.io>
2024-03-01 11:53:05 +0000

    trying smaller space again

--- example/TestBenchmarks/simpleMemoryAllocAndFree/ContigTest/test.c
+++ example/TestBenchmarks/simpleMemoryAllocAndFree/ContigTest/test.c
@@ -22,10 +22,10 @@
 int main(int argc, char **argv)
 { 
   // 1 GB 
-  size_t size = 1000000000;
+  // size_t size = 1000000000;
   
   // smaller size
-  //size_t size = 1000;
+  size_t size = 1000000;
 
   // Custom malloc
   int fd = open
@@ -34,98 +34,102 @@ int main(int argc, char **argv)
 		perror("open");
 		exit(EXIT_FAILURE);
 	}
-  struct stat st;
-  stat(FILENAME, &st);
-  size = st.st_size;
-  printf(size);
 
-// 	off_t offset = 0; // offset to seek to.
-
-// 	if (ftruncate(fd, size) < 0) {
-// 		perror("ftruncate");
-// 		close(fd);
-// 		exit(EXIT_FAILURE);
-// 	}
-
-// 	int *ptr = mmap(NULL, size,
-// 	                         PROT_READ | PROT_WRITE, MAP_SHARED,
-// 	                         fd, offset);
-// 	if(ptr == MAP_FAILED)
-// 	{
-// 		perror("mmap");
-// 		exit(EXIT_FAILURE);
-// 	}
-
-//     *ptr = size;
+  // Cannot read in security exception 
+  // on reads
+  // struct stat st;
+  // stat(FILENAME, &st);
+  // size = st.st_size;
+  // printf(size);
 
 
-//   printf("Successfully malloc'd %zu bytes at addr %p\n", size, ptr);
-//   assert(ptr != NULL);
+	off_t offset = 0; // offset to seek to.
 
-//   // Custom free 
-//   // int *pt = ptr;
-//   //   // size_t size;
-//   //   --pt;
-//   //   size = *pt;
-//   //   if(munmap(pt, size) == -1)
-// 	// {
-// 	// 	perror("munmap");
-// 	// 	exit(EXIT_FAILURE);
-//   // }
+	if (ftruncate(fd, size) < 0) {
+		perror("ftruncate");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
 
-//   printf("Successfully free'd %zu bytes from addr %p\n", size, ptr);
-//   return 0;
-// }
+	int *ptr = mmap(NULL, size,
+	                         PROT_READ | PROT_WRITE, MAP_SHARED,
+	                         fd, offset);
+	if(ptr == MAP_FAILED)
+	{
+		perror("mmap");
+		exit(EXIT_FAILURE);
+	}
 
-// static inline void *MALLOC(size_t size)
-// {
-// //    void * temp = malloc(size);
-// //    assert(temp);
-// //    return temp;
-
-// // Ensuring malloc this is mmap and phyiscally contigous
-//     int fd = open
-// 	(FILENAME, O_RDWR | O_CREAT, 0600);
-// 	if (fd < 0) {
-// 		perror("open");
-// 		exit(EXIT_FAILURE);
-// 	}
-
-// 	off_t offset = 0; // offset to seek to.
-
-// 	if (ftruncate(fd, size) < 0) {
-// 		perror("ftruncate");
-// 		close(fd);
-// 		exit(EXIT_FAILURE);
-// 	}
-
-// 	int *ptr = mmap(NULL, size,
-// 	                         PROT_READ | PROT_WRITE, MAP_SHARED,
-// 	                         fd, offset);
-// 	if(ptr == MAP_FAILED)
-// 	{
-// 		perror("mmap");
-// 		exit(EXIT_FAILURE);
-// 	}
-
-//     *ptr = size;
-
-//     return ptr;
-//    // void *ptr = malloc(size);
-//    // return ptr;
-// }
+    *ptr = size;
 
 
-// static inline void *FREE(void *ptr)
-// {
-//     int *pt = ptr;
-//     size_t size;
-//     --pt;
-//     size = *pt;
-//     if(munmap(pt, size) == -1)
-// 	{
-// 		perror("munmap");
-// 		exit(EXIT_FAILURE);
-// 	}
-//    // free(ptr);
+  printf("Successfully malloc'd %zu bytes at addr %p\n", size, ptr);
+  assert(ptr != NULL);
+
+  // Custom free 
+  // int *pt = ptr;
+  //   // size_t size;
+  //   --pt;
+  //   size = *pt;
+  //   if(munmap(pt, size) == -1)
+	// {
+	// 	perror("munmap");
+	// 	exit(EXIT_FAILURE);
+  // }
+
+  printf("Successfully free'd %zu bytes from addr %p\n", size, ptr);
+  return 0;
+}
+
+static inline void *MALLOC(size_t size)
+{
+//    void * temp = malloc(size);
+//    assert(temp);
+//    return temp;
+
+// Ensuring malloc this is mmap and phyiscally contigous
+    int fd = open
+	(FILENAME, O_RDWR | O_CREAT, 0600);
+	if (fd < 0) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = 0; // offset to seek to.
+
+	if (ftruncate(fd, size) < 0) {
+		perror("ftruncate");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	int *ptr = mmap(NULL, size,
+	                         PROT_READ | PROT_WRITE, MAP_SHARED,
+	                         fd, offset);
+	if(ptr == MAP_FAILED)
+	{
+		perror("mmap");
+		exit(EXIT_FAILURE);
+	}
+
+    *ptr = size;
+
+    return ptr;
+   // void *ptr = malloc(size);
+   // return ptr;
+}
+
+
+static inline void *FREE(void *ptr)
+{
+    int *pt = ptr;
+    size_t size;
+    --pt;
+    size = *pt;
+    if(munmap(pt, size) == -1)
+	{
+		perror("munmap");
+		exit(EXIT_FAILURE);
+	}
+   // free(ptr);
 }
